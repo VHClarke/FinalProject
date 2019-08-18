5200aebcaac63e02432d64e36eef2e346967a9ca
Resilient Coders 13 <resilientcoders13@Resilients-MacBook-Pro.local>
2019-08-18 12:02:10 -0400

    DB name change

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0-jwqok.mongodb.net/foodie?retryWrites=true&w=majority`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'foodie'
+    'url' : `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0-jwqok.mongodb.net/${process.env.BD_NAME}?retryWrites=true&w=majority`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': process.env.BD_NAME
 };
