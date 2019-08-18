54ace29525e0970d8d7a95711e3f82ef9d1dee3f
Resilient Coders 13 <resilientcoders13@Resilients-MacBook-Pro.local>
2019-08-18 18:25:53 -0400

    add gitignore

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 // config/database.js
 module.exports = {
-
     'url' : `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0-jwqok.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'dbName': process.env.DB_NAME
 };
